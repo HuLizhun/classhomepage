17efb16d5a08a85a24692779f0de2853b5dc0dee
HuLizhun <529907083@qq.com>
2018-04-03 11:41:59 +0800

    添加service实现类

---
+++ src/main/java/com/textile083/serviceImpl/SearchServiceImpl.java
@@ -0,0 +1,29 @@
+package com.textile083.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.textile083.dao.ArticleDAO;
+import com.textile083.entity.Article;
+import com.textile083.service.SearchService;
+
+@Service("searchService")
+public class SearchServiceImpl implements SearchService {
+	@Autowired
+	private ArticleDAO articleDao;
+
+	public boolean doExit(String exit) {
+		if (exit != null && exit.equals("退出")) {
+			return true;
+		}
+		return false;
+	}
+
+	public Article doSearch(String context) {
+		if (context != null) {
+			Article article = (Article) articleDao.queryArticleByTitle(context);
+			return article;
+		}
+		return null;
+	}
+}
