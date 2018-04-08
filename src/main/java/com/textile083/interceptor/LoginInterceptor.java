06a1029e53077eeed9182d5d6bf1a0b780d83d40
HuLizhun <529907083@qq.com>
2018-04-08 17:08:29 +0800

    修改LoginInterceptor的代码

--- src/main/java/com/textile083/interceptor/LoginInterceptor.java
+++ src/main/java/com/textile083/interceptor/LoginInterceptor.java
@@ -12,24 +12,18 @@ public class LoginInterceptor implements HandlerInterceptor {
 
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		// TODO Auto-generated method stub
-//		User user=(User) request.getSession().getAttribute("user");
-//		if(user==null){
-//			request.getRequestDispatcher("/WEB-INF/jsps/login.jsp").forward(request, response);
-//			return false;
-//		}else{
-//			return true;
-//		}
-		return true;
+		User user=(User) request.getSession().getAttribute("user");
+		String name=(String)request.getParameter("name");
+		if(name==null&&user==null){
+			request.getRequestDispatcher("/WEB-INF/jsps/login.jsp").forward(request, response);
+			return false;
+		}else{
+			return true;
+		}
 	}
 
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		User user=(User) request.getSession().getAttribute("user");
-		if(user==null){
-			modelAndView.setViewName("login");
-		}
-		
+			ModelAndView modelAndView) throws Exception {	
 	}
 
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
